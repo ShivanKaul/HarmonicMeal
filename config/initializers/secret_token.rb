6803aa99699e5baa989a748a15af0f37704e2df7
Shivan <shivankaul.1993@gmail.com>
2014-05-24 11:14:32 -0400

    Hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HarmonicMeal::Application.config.secret_token = 'e9aec1737989a8e9e0a706ce5beec104b59587e3f3f1cf93d52fb10ae05e7cd2a34733572e7495dc788cb4a4ab958a8c49af23964feb6c88137a7b910274a233'
